f3604b77a9c11e288b44db005af6d4ec37d8e95a
Henrique Strada <henriquestradax@gmail.com>
2023-08-12 20:14:48 -0300

    Commit calculadora

--- src/main/java/com/example/calculadora_prestativa/Calculadora.java
+++ src/main/java/com/example/calculadora_prestativa/Calculadora.java
@@ -21,7 +21,7 @@ public class Calculadora{
         return refazer;
     }
     public static void mostrarImagem() {
-        JFrame frame = new JFrame("Eu sabo muito, vdd manja dms mlk");
+        JFrame frame = new JFrame("Link do gitHub: https://github.com/HenriqueStrada/CalculadoraEuSabo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ImageIcon imageIcon = new ImageIcon("src/main/resources/EuSabo.jpg");
         JLabel label = new JLabel(imageIcon);
@@ -43,6 +43,10 @@ public class Calculadora{
                 (3) Multiplicação.
                 (4) Divisão.
                 (5) Raiz.
+                (6) Circunferencia circulo.
+                (7) Calcular hipotenusa.
+                (8) Calculadora de Unidades de medida.
+                (9) Calculadora de Angulo Seno, Cosseno, Tangente.
                 """);
 
         String resposta = JOptionPane.showInputDialog("Responda qual conta quer fazer:");
@@ -50,43 +54,132 @@ public class Calculadora{
 
         if(respostanum == 1){
             double[] numeros = pegaValores();
+            JOptionPane.showMessageDialog(null, "A soma de " + numeros[0] + " por " + numeros[1] + "é " + (numeros[0] + numeros[1]));
             System.out.println("O resultado da soma é: " + (numeros[0]+ numeros[1]));
-            mostrarImagem();
             System.out.println("Link do gitHub: https://github.com/HenriqueStrada/CalculadoraEuSabo");
             int refazerres = pergunteRefaz();
-            if (refazerres == 1) {refazer = 1;} else {refazer = 0;}
+            if (refazerres == 1) {refazer = 1;} else {mostrarImagem(); refazer = 0;}
         } else if (respostanum == 2){
             double[] numeros = pegaValores();
+            JOptionPane.showMessageDialog(null, "A subtração de " + numeros[0] + " por " + numeros[1] + "é " + (numeros[0] - numeros[1]));
             System.out.println("O resultado da subtração é: " + (numeros[0] - numeros[1]));
-            mostrarImagem();
             System.out.println("Link do gitHub: https://github.com/HenriqueStrada/CalculadoraEuSabo");
             int refazerres = pergunteRefaz();
-            if (refazerres == 1) {refazer = 1;} else {refazer = 0;}
+            if (refazerres == 1) {refazer = 1;} else {mostrarImagem(); refazer = 0;}
         } else if (respostanum == 3){
             double[] numeros = pegaValores();
+            JOptionPane.showMessageDialog(null, "A multiplicação de " + numeros[0] + " por " + numeros[1] + "é " + (numeros[0] * numeros[1]));
             System.out.println("O resultado da multiplicação é: " + (numeros[0] * numeros[1]));
-            mostrarImagem();
             System.out.println("Link do gitHub: https://github.com/HenriqueStrada/CalculadoraEuSabo");
             int refazerres = pergunteRefaz();
-            if (refazerres == 1) {refazer = 1;} else {refazer = 0;}
+            if (refazerres == 1) {refazer = 1;} else {mostrarImagem(); refazer = 0;}
         } else if (respostanum == 4){
             double[] numeros = pegaValores();
+            JOptionPane.showMessageDialog(null, "A divisão de " + numeros[0] + " por " + numeros[1] + "é " + (numeros[0] / numeros[1]));
             System.out.println("O resultado da divisão é: " + (numeros[0] / numeros[1]));
-            mostrarImagem();
             System.out.println("Link do gitHub: https://github.com/HenriqueStrada/CalculadoraEuSabo");
             int refazerres = pergunteRefaz();
-            if (refazerres == 1) {refazer = 1;} else {refazer = 0;}
+            if (refazerres == 1) {refazer = 1;} else {mostrarImagem(); refazer = 0;}
         } else if (respostanum == 5) {
             String valorUm = JOptionPane.showInputDialog("Digite o valor do numero que quer descobrir a raiz:");
-            int raiz = Integer.parseInt(valorUm);
+            double raiz = Double.parseDouble(valorUm);
+            JOptionPane.showMessageDialog(null, "A raiz de " + raiz + " é: " + Math.sqrt(raiz));
             System.out.println("A raiz de " + raiz + " é: " + Math.sqrt(raiz));
-            mostrarImagem();
             System.out.println("Link do gitHub: https://github.com/HenriqueStrada/CalculadoraEuSabo");
             int refazerres = pergunteRefaz();
-            if (refazerres == 1) {refazer = 1;} else {refazer = 0;}
-        } else {
+            if (refazerres == 1) {refazer = 1;} else {mostrarImagem(); refazer = 0;}
+        } else if (respostanum == 6) {
+            Object[] opcoes = {"Raio", "Diametro"};
+            int escolha = JOptionPane.showOptionDialog(null, "Você possui o valor do diametro, ou do raio?", "Opções",
+                    JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcoes, opcoes[0]);
+            if(escolha == 0){
+                String valorRaio = JOptionPane.showInputDialog("Digite o valor do raio de seu circulo.");
+                double raio = Double.parseDouble(valorRaio);
+                double raioDim = raio * 2;
+                JOptionPane.showMessageDialog(null, "A circunferencia de um circulo com raio igual a " + raio + " é: " + (raioDim * Math.PI));
+            } else {
+                String valorDiametro = JOptionPane.showInputDialog("Digite o valor do diametro de seu circulo");
+                double diametro = Double.parseDouble(valorDiametro);
+                JOptionPane.showMessageDialog(null, "A circunferencia de um circulo com diametro igual a " + diametro + " é: " + (diametro * Math.PI));
+            } int refazerres = pergunteRefaz();
+            if (refazerres == 1) {refazer = 1;} else {mostrarImagem(); refazer = 0;}
+        } else if (respostanum == 7){
+            String valorCateto = JOptionPane.showInputDialog("Digite o valor do primeiro cateto de seu triangulo");
+            double catetoUm = Double.parseDouble(valorCateto);
+            String valorCatetoDois = JOptionPane.showInputDialog("Digite o valor do segundo cateto de seu triangulo");
+            double catetoDois = Double.parseDouble(valorCatetoDois);
+            double somCatQuard = (catetoUm * catetoUm) + (catetoDois * catetoDois);
+            JOptionPane.showMessageDialog(null, "A hipotenusa do seu triangulo é: " + Math.sqrt(somCatQuard));
+            int refazerres = pergunteRefaz();
+            if (refazerres == 1) {refazer = 1;} else {mostrarImagem(); refazer = 0;}
+        } else if(respostanum == 8){
+            Object[] opcoes = {"Metro para pes", "Pes para metros", "Quilômetros para milhas", "Milhas para quilômetros", "Polegadas para Centimetro", "Centimetro para polegada"};
+            int escolha = JOptionPane.showOptionDialog(null, "Escolha uma opção", "Opções de medidas", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcoes, opcoes[0]);
+            switch (escolha){
+                case 0:
+                    String valorMetro = JOptionPane.showInputDialog("Digite quantos metros quer converter para pés: ");
+                    double metros = Double.parseDouble(valorMetro);
+                    JOptionPane.showMessageDialog(null, "O valor de metros convertidos para pés são de: " + (metros * 3.28084) + " pés");
+                    break;
+                case 1:
+                    String valorPes = JOptionPane.showInputDialog("Digite quantos pés quer converter para metros: ");
+                    double pes = Double.parseDouble(valorPes);
+                    JOptionPane.showMessageDialog(null, "O valor de pés convertidos para metros são de: " + (pes * 0.3048) + " metros");
+                    break;
+                case 2:
+                    String valorKm = JOptionPane.showInputDialog("Digite quantos quilômetros quer converter para milhas: ");
+                    double km = Double.parseDouble(valorKm);
+                    JOptionPane.showMessageDialog(null, "O valor de quilômetros convertidos para milhas são de: " + (km * 0.621371) + " milhas");
+                    break;
+                case 3:
+                    String valorMilha = JOptionPane.showInputDialog("Digite quantas milhas quer converter para quilômetros: ");
+                    double milha = Double.parseDouble(valorMilha);
+                    JOptionPane.showMessageDialog(null, "O valor de milhas convertidas para quilômetros são de: " + (milha * 1.60934) + " quilômetros");
+                    break;
+                case 4:
+                    String valorPolegada = JOptionPane.showInputDialog("Digite quantas polegadas quer converter para centimetros: ");
+                    double polegada = Double.parseDouble(valorPolegada);
+                    JOptionPane.showMessageDialog(null, "O valor de polegadas convertidas para centimetros são de: " + (polegada * 2.54) + " centimetros");
+                    break;
+                case 5:
+                    String valorCentimetro = JOptionPane.showInputDialog("Digite quantos centimetros quer converter para polegadas: ");
+                    double centimetro = Double.parseDouble(valorCentimetro);
+                    JOptionPane.showMessageDialog(null, "O valor de centimetros convertidas para polegadas são de: " + (centimetro * 0.393701) + " polegadas");
+                    break;
+            }
+            int refazerres = pergunteRefaz();
+            if (refazerres == 1) {refazer = 1;} else {mostrarImagem(); refazer = 0;}
+        }else if (respostanum == 9){
+            Object[] opcoes = {"Seno", "Cosseno", "Tangente"};
+            int escolha = JOptionPane.showOptionDialog(null, "Escolha o que quer descobrir", "Medidas de angulo", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcoes, opcoes[0]);
+            switch (escolha) {
+                case 0:
+                    String valorCatOpost = JOptionPane.showInputDialog("Digite o cateto oposto da hipotenusa: ");
+                    double catopost = Double.parseDouble(valorCatOpost);
+                    String valorHipotenusa = JOptionPane.showInputDialog("Digite o valor da hipotenusa: ");
+                    double catohipo = Double.parseDouble(valorHipotenusa);
+                    JOptionPane.showMessageDialog(null, "O valor do Seno é: " + (catopost / catohipo) + " Graus");
+                    break;
+                case 1:
+                    String valorCateAdja = JOptionPane.showInputDialog("Digite o cateto adjacente da hipotenusa: ");
+                    double catoAdj = Double.parseDouble(valorCateAdja);
+                    String valorHipotenus = JOptionPane.showInputDialog("Digite o valor da hipotenusa: ");
+                    double catohipot = Double.parseDouble(valorHipotenus);
+                    JOptionPane.showMessageDialog(null, "O valor do Cosseno é: " + (catoAdj / catohipot) + " Graus");
+                    break;
+                case 2:
+                    String valorCateOposto = JOptionPane.showInputDialog("Digite o cateto oposto da hipotenusa: ");
+                    double catoOposto = Double.parseDouble(valorCateOposto);
+                    String valorAdjas = JOptionPane.showInputDialog("Digite o cateto adjacente da hipotenusa: ");
+                    double catoAdjas = Double.parseDouble(valorAdjas);
+                    JOptionPane.showMessageDialog(null, "O valor da Tangente é: " + (catoOposto / catoAdjas) + " Graus");
+                    break;
+            }
+            int refazerres = pergunteRefaz();
+            if (refazerres == 1) {refazer = 1;} else {mostrarImagem(); refazer = 0;}
+        }else {
+            JOptionPane.showMessageDialog(null, "Coloque um valor valido!");
             System.out.println("Coloque um valor valido!");
-            mostrarImagem();
             System.out.println("Link do gitHub: https://github.com/HenriqueStrada/CalculadoraEuSabo");
             refazer = 1;
             }
@@ -94,4 +187,3 @@ public class Calculadora{
         System.out.println("Link do gitHub: https://github.com/HenriqueStrada/CalculadoraEuSabo");
     }
 }
-
