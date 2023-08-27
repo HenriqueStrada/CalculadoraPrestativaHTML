746f3cbb6b1ad475b9c28940b505aa42b0f15954
Henrique Strada <henriquestradax@gmail.com>
2023-08-27 13:36:16 -0300

    Commit calculadora

---
+++ src/main/java/com/example/calculadora_prestativa/Main.java
@@ -0,0 +1,11 @@
+package com.example.calculadora_prestativa;
+import static com.example.calculadora_prestativa.util.Calcula.Calculadora;
+import static com.example.calculadora_prestativa.util.Uteis.mostrarImagem;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculadora();
+        mostrarImagem();
+        System.out.println("Link do gitHub: https://github.com/HenriqueStrada/CalculadoraEuSabo");
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,6 @@ module com.example.calculadora_prestativa {
 
     opens com.example.calculadora_prestativa to javafx.fxml;
     exports com.example.calculadora_prestativa;
+    exports com.example.calculadora_prestativa.util;
+    opens com.example.calculadora_prestativa.util to javafx.fxml;
 }
